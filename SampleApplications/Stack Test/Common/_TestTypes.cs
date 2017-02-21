481dec5ae269b1ed4b8ddf1c38e90cbcc9fd6d18
S-Ivanov <s.v.ivanov@bk.ru>
2017-02-21 23:23:15 +0300

    Портирование в .Net 4.0

---
+++ SampleApplications/Stack Test/Common/_TestTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Opc.Ua.StackTest
+{
+    public class CompositeTestType
+    {
+    }
+
+    public enum EnumeratedTestType : int
+    {
+        Red_1 = 0,
+        Yellow_4 = 1,
+        Green_5 = 2,
+    }
+}
